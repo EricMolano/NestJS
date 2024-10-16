04af328050c00dee8691bbbc424ab5303a6f13e1
Aprendiz <Aprendiz@BOGDFPCGMF4147.FormacionSena.RED>
2024-10-16 12:07:31 -0500

    feat: se inicia

---
+++ src/entities/Student.entity.ts
@@ -0,0 +1,14 @@
+export class Student {
+
+    constructor ( id: number , nom: string , edad: number , c: string ) {
+                this.tarjeta = id;
+                this.nombre = nom ;
+                this.edad = edad; 
+                this.ciudad = c ;
+}
+
+    tarjeta:number;
+    nombre: string;
+    edad: number;
+    ciudad: string
+}
