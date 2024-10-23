84053c673d887933a2618e4667524a0217ffedf9
Aprendiz <Aprendiz@BOGDFPCGMF4147.FormacionSena.RED>
2024-10-23 08:04:02 -0500

    feat: arreglos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,40 +1,10 @@
 import { Controller, Get , Param , Query} from '@nestjs/common';
 import { AppService } from './app.service';
-import { Student } from './entities/Student.entity';
 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  //Endpoint: puerta del software
-  //        -Acepta peticiones de clientes bajo una url Semantica
-  //        -Retornar el(los) datos esperados al cliente 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  //Segundo endpint - ESTRUCTURAR UN ENDPOINT
-  //                1.  Verbo (metodo) Http sobre el cual recibir
-  //                Metodos disponibles: GET, POST, PUT, DELETE
-  //                2. Firma de metodo a ejecutar 
-  //                cuando se invoque el endpoint
-
-  @Get("/ficha")
-  getFicha(): string {
-    return "endpoint de la ficha 2902093"
-
-  }
-
-
-  @Get("/tarjeta/:id/ciudad/:ciudad")
-  tarjeta(@Param("id") id:string , 
-          @Param("ciudad") ciudad :string,
-          @Query("nombre") nombre : string,
-          @Query("edad") edad:number ):Student {
-    return new Student (+id, nombre, edad , ciudad);
-  }
 
   
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BootcampController } from './controllers/bootcamp.controller';
+import { BootcampsModule } from './bootcamps/bootcamps.module';
+import { CoursesModule } from './courses/courses.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BootcampController],
+  imports: [BootcampsModule, CoursesModule, ReviewsModule, UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
